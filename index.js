5deb57bfb64c4314589a193ee5625aeea84a8040
MLaven91 <maxlavender02@gmail.com>
2025-03-09 21:54:55 +0000

    add dynamic header links and refactor social links creation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = [
+    const socialLinks = [
         {
             name: "Instagram",
             url: "./",
@@ -17,18 +17,43 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     ];
 
-    const container = document.getElementById("social-links");
+    const headerLinks = [
+        {
+            name: "Home",
+            url: "./",
+        },
+        {
+            name: "About",
+            url: "./",
+        },
+        {
+            name: "Blog",
+            url: "./",
+        },
+    ];
 
-    links.forEach(link => {
+    function createAnchor(link, className, icon = '') {
         const anchor = document.createElement("a");
-        anchor.href = link.url
-        anchor.className = "flex w-fit items-center rounded-full bg-gray-100 px-3 py-1 hover:bg-gray-200";
-        anchor.innerHTML = `
-            ${link.name}
+        anchor.href = link.url;
+        anchor.className = className;
+        anchor.innerHTML = `${link.name} ${icon}`;
+        return anchor;
+    }
+
+    const socialContainer = document.getElementById("social-links");
+    socialLinks.forEach(link => {
+        const icon = `
         <svg class="h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
             ${link.icon}
         </svg>
             `;
-        container.appendChild(anchor);
+        const anchor = createAnchor(link, "flex items-center bg-gray-100 rounded-full px-3 py-1.5 text-sm transition-colors hover:text-gray-900 hover:bg-gray-200", icon);
+        socialContainer.appendChild(anchor);
+    });
+
+    const headerContainer = document.getElementById("header-links");
+    headerLinks.forEach(link => {
+        const anchor = createAnchor(link, "text-gray-700 rounded-lg px-3 py-1.5 text-sm transition-colors hover:text-gray-900 hover:bg-gray-100");
+        headerContainer.appendChild(anchor);
     });
 });
